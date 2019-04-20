c78fe2cffaf6e6725e9d006738c398c92f3e354b
miinaw <yong.abema@gmail.com>
2019-04-21 03:41:54 +0900

    fix:graph.py => 一日の工数と工数の回数がわかるようにする、日付順にソート

--- graph.py
+++ graph.py
@@ -5,11 +5,14 @@ import pandas as pd
 
 def create_graph():
   df = pd.read_csv('toggl_data.csv', sep=',')
-  x = df['date']
-  y = df['time']
+  df_s = df.sort_values('date', ascending=True)
+  print(df_s)
+  x = df_s['date']
+  y = df_s['time']
 
   plt.title(sys.argv[1])
   plt.xlabel('date')
   plt.ylabel('time(m)')
-  plt.plot(x, y, marker="o", color = "red", linestyle = "--")
+  plt.bar(x, y, color = "blue") # 一日の工数
+  plt.plot(x, y, marker="o", color = "red", linestyle = "None") # 一日の工数の回数
   plt.show()
