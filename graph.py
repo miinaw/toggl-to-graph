f41761816d5c26ced3e6021b7f39cfddbdc2f5b0
miinaw <yong.abema@gmail.com>
2019-04-21 02:44:19 +0900

    fix:graph.py => グラフ作成

---
+++ graph.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import sys
+import pandas as pd
+
+df = pd.read_csv('toggl_data.csv', sep=',')
+x = df['date']
+y = df['time']
+print(x)
+print(y)
+
+plt.plot(x, y, marker="o", color = "red", linestyle = "--")
+plt.show()
