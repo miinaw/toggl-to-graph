7161b655314f6ce1215ce19e8d972731687f9647
miinaw <yong.abema@gmail.com>
2019-04-21 02:58:10 +0900

    fix:graph.py => グラフタイトル追加

--- graph.py
+++ graph.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 import sys
 import pandas as pd
 
-df = pd.read_csv('toggl_data.csv', sep=',')
-x = df['date']
-y = df['time']
-print(x)
-print(y)
 
-plt.xlabel('date')
-plt.ylabel('time(m)')
-plt.plot(x, y, marker="o", color = "red", linestyle = "--")
-plt.show()
+def create_graph():
+  df = pd.read_csv('toggl_data.csv', sep=',')
+  x = df['date']
+  y = df['time']
+
+  plt.title(sys.argv[1])
+  plt.xlabel('date')
+  plt.ylabel('time(m)')
+  plt.plot(x, y, marker="o", color = "red", linestyle = "--")
+  plt.show()
